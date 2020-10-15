ebc87d7758a992396f7e549669d02a48eca4ebdc
vini <amorimm.viniciuss@gmail.com>
2020-10-15 14:16:16 -0300

    React Navigation

---
+++ src/routes.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Main from './pages/Main';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Main"
+          component={Main}
+          options={{
+            title: 'Usuários',
+            headerStyle: {
+              backgroundColor: '#7159c1',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20,
+              flex: 1,
+              textAlign: 'center',
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
